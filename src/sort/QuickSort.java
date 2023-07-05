db5819893e432dfc371973d9d4f5d1860c71ebc1
Kimhannah <skqkdldhf98@naver.com>
2023-07-05 16:17:55 +0900

    중복값 포함한 배열에 대한 퀵정렬 v2

--- src/sort/QuickSort.java
+++ src/sort/QuickSort.java
@@ -1,5 +1,7 @@
 package sort;
 
+import java.util.Arrays;
+
 public class QuickSort {
   private static void swap(int[] arr, int a, int b) {
     int tmp = arr[a];
@@ -9,36 +11,41 @@ public class QuickSort {
 
   private static int partition(int[] arr, int left, int right) {
     int pivot = arr[left];  // first element pivot
-    int index = left;
-    boolean flag = true;
+    int index = left++;
 
     while (left < right) {
-      if (flag == true) {  // right -> left
-        if (arr[right] < pivot) {
-          swap(arr, right, index);
-          index = right;
-          flag = false;
-        } else {
-          --right;
-        }
-      }
-      else if (flag == false) {   // left -> right
-        if (arr[left] > pivot) {
-          swap(arr, left, index);
-          index = left;
-          flag = true;
-        } else {
-          ++left;
-        }
-      }
+      while (left < right && arr[right] >= pivot)
+        --right;
+      while (left < right && arr[left] < pivot)
+        ++left;
+      swap(arr, left, right);
+    }
+
+    // left >= right
+    if (index < right && pivot > arr[right]) {
+      swap(arr, index, right);
+      index = right;
     }
     return index;
   }
   public static void sort(int[] arr, int left, int right) {
     if (left < right) {
       int index = partition(arr, left, right);
+      System.out.println(Arrays.toString(arr));
       sort(arr, left, index - 1);
       sort(arr, index + 1, right);
     }
+  }
+
+  public static void main(String[] args) {
+    int[] arr = new int[]{5, 4, 3, 2, 1, 3, 4, 5, 6, 7, 8, 1, 10, 11, 12, 10, 0, 1, -1, 20, 23, -2, 0, -2};
+
+    System.out.println("=== Before ===");
+    System.out.println(Arrays.toString(arr));
+
+    QuickSort.sort(arr, 0, arr.length - 1);
+
+    System.out.println("=== After ===");
+    System.out.println(Arrays.toString(arr));
   }
 }
