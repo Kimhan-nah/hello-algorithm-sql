58eff5ce1cd5f9b0c553a12fd6fb8c9e7fe476f6
Kimhannah <skqkdldhf98@naver.com>
2023-07-12 17:10:10 +0900

    Update Quick Sort : pivot 위치 고정 후 마지막에 swap으로 변경

--- src/sort/QuickSort.java
+++ src/sort/QuickSort.java
@@ -14,29 +14,36 @@ public class QuickSort {
     int pivot = arr[mid];
 
     while (left < right) {
-      while (arr[left] < pivot)
+      while (left < right && arr[left] <= pivot)
         ++left;
-      while (arr[right] > pivot)
+      while (left < right && (right == mid || arr[right] > pivot))
         --right;
-
-      swap(arr, left, right);
+      if (left < right)
+        swap(arr, left, right);
     }
 
-    return right;
+    // left == right
+    int index = right;
+    if (mid < right && arr[mid] < arr[right])
+        index = right - 1;
+    else if (mid > right && arr[mid] > arr[right])
+        index = right + 1;
+    swap(arr, mid, index);
+    return index;
   }
   public static void sort(int[] arr, int left, int right) {
     if (left < right) {
       int index = partition(arr, left, right);
-      System.out.println("index : " + index);
-      System.out.println(Arrays.toString(arr));
       sort(arr, left, index - 1);
       sort(arr, index + 1, right);
     }
   }
 
   public static void main(String[] args) {
-    int[] arr = new int[]{5,1,3,2,4,6, -1, -2, 0, 9, 10};
+//    int[] arr = new int[]{5,1,3,2,4,6, -1, -2, 0, 9, 10};
+//    int[] arr = new int[]{8, 9, 5, 4, 3, 2, 1, 3, 4, 5, 6, 7,6};
 //    int[] arr = new int[]{5, 4, 3, 2, 1, 3, 4, 5, 6, 7, 8, 1, 10, 11, 12, 10, 0, 1, -1, 20, 23, -2, 0, -2};
+    int[] arr = new int[]{0,0,10,12,13,1,2,8,9,6,7,20,21,22,15,13,14,15,20};
 
     System.out.println("=== Before ===");
     System.out.println(Arrays.toString(arr));
