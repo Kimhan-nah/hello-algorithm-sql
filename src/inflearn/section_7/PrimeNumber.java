4fc738f2d6d146b2db7ffba827687161aa81301a
Kimhannah <skqkdldhf98@naver.com>
2023-07-18 15:09:02 +0900

    시간 초과 해결

--- src/inflearn/section_7/PrimeNumber.java
+++ src/inflearn/section_7/PrimeNumber.java
@@ -1,19 +1,24 @@
 package inflearn.section_7;
 
-public class PrimeNumber {
-  private static int DFS(int num) {
-    if (num < 2)
-      return 0;
-    for (int i = 2; i <= num / 2; ++i) {
-      if (num % i == 0) {
-        return DFS(num - 1);
-      }
-    }
-    return 1 + DFS(num - 1);
-  }
+import java.util.Scanner;
 
+public class PrimeNumber {
+  private static int solution(int n) {
+    int answer = 0;
+    int[] ch = new int[n + 1];
+
+    for (int i = 2; i <= n; ++i) {
+      if (ch[i] == 0)
+        ++answer;
+      for (int j = 2; i * j < ch.length; ++j)
+        ch[i * j] = 1;
+    }
+    return answer;
+  }
   public static void main(String[] args) {
-    System.out.println(DFS(20));
+    Scanner sc = new Scanner(System.in);
+    int n = sc.nextInt();
+    System.out.println(solution(n));
 
   }
 
