a2ee48669c8d8b0d879fe14a336bf0f7a2d9aeec
Kimhannah <skqkdldhf98@naver.com>
2023-07-19 12:16:40 +0900

    Add Inflearn Section 7-5 이진트리 순회(DFS)

---
+++ src/inflearn/section_7/BinarySearchTree.java
@@ -0,0 +1,35 @@
+package inflearn.section_7;
+
+class Node {
+  int data;
+  Node left, right;
+  public Node(int val) {
+    data= val;
+    left = right = null;
+  }
+}
+
+public class BinarySearchTree {
+  Node root;
+
+  public void DFS(Node root) {
+    if (root == null)
+      return ;
+    DFS(root.left);
+    System.out.print(root.data + " ");
+    DFS(root.right);
+  }
+
+  public static void main(String[] args) {
+    BinarySearchTree tree = new BinarySearchTree();
+    tree.root = new Node(1);
+    tree.root.left = new Node(2);
+    tree.root.right= new Node(3);
+    tree.root.left.left = new Node(4);
+    tree.root.left.right= new Node(5);
+    tree.root.right.left= new Node(6);
+    tree.root.right.right= new Node(7);
+
+    tree.DFS(tree.root);
+  }
+}
