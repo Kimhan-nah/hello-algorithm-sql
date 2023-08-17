b3e79c573191c138f4763b1ef85a864bd543ef32
Kimhannah <skqkdldhf98@naver.com>
2023-08-17 13:41:18 +0900

    :bulb: Modify Inflearn 7. 조합수 메모제이션 적용

--- src/inflearn/section_8/Combination.java
+++ src/inflearn/section_8/Combination.java
@@ -3,10 +3,13 @@ package inflearn.section_8;
 import java.util.*;
 
 public class Combination {
+  static int[][] dy = new int[35][35];
+
   private static int solution(int n, int r) {
+    if (dy[n][r] > 0) return dy[n][r];
     if (r == 1) return n;
     if (r == 0 || r == n) return 1;
-    return solution(n-1, r-1) + solution(n-1, r);
+    return dy[n][r] = solution(n-1, r-1) + solution(n-1, r);
   }
 
   public static void main(String[] args) {
