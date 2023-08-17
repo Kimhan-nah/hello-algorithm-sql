87b469ac6b11579bae73279db99e9bee8d516b1a
Kimhannah <skqkdldhf98@naver.com>
2023-08-17 13:22:06 +0900

    :seedling: Add Inflearn 조합의 경우수 (메모이제이션)

---
+++ src/inflearn/section_8/Combination.java
@@ -0,0 +1,19 @@
+package inflearn.section_8;
+
+import java.util.*;
+
+public class Combination {
+  private static int solution(int n, int r) {
+    if (r == 1) return n;
+    if (r == 0 || r == n) return 1;
+    return solution(n-1, r-1) + solution(n-1, r);
+  }
+
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    int n = sc.nextInt();
+    int m = sc.nextInt();
+    int answer = solution(n, m);
+    System.out.println(answer);
+  }
+}
