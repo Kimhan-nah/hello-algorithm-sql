13fe619337f623f1b4bbc6dea38c4548d4fc1b2a
Kimhannah <skqkdldhf98@naver.com>
2023-09-05 14:28:24 +0900

    :recycle: Modify 5. 중복확인 with Arrays.sort() API

--- src/inflearn/section_6/DuplciateVerification.java
+++ src/inflearn/section_6/DuplciateVerification.java
@@ -5,10 +5,9 @@ import java.io.*;
 
 public class DuplciateVerification {
   private static char solution(int[] arr) {
+    Arrays.sort(arr);
     for (int i = 0; i < arr.length - 1; ++i) {
-      for (int j = i + 1; j < arr.length; ++j) {
-        if (arr[i] == arr[j]) return 'D';
-      }
+      if (arr[i] == arr[i + 1]) return 'D';
     }
     return 'U';
   }
