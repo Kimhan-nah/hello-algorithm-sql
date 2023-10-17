4615f5d741f5ee0a57c474e12e85a62890154f09
Kimhannah <skqkdldhf98@naver.com>
2023-10-17 19:33:28 +0900

    :bulb: BaekJoon 1453번: 1로 만들기 (dp)
    시간 단축

--- src/baekjoon/dynamic_programming/BJ1463.java
+++ src/baekjoon/dynamic_programming/BJ1463.java
@@ -1,24 +1,31 @@
 package baekjoon.dynamic_programming;
 
 import java.io.*;
+import java.util.Arrays;
 
 public class BJ1463 {
   static int answer = Integer.MAX_VALUE;
+  static int[] dp;
 
   private static void solution(int N, int level) {
     if (N == 1) {
       answer = Math.min(answer, level);
       return ;
     }
+    if (dp[N] < level) return ;
 
-    if (N % 3 == 0) solution(N / 3, level + 1);
-    if (N % 2 == 0) solution(N / 2, level + 1);
-    solution(N - 1, level + 1);
+    dp[N] = level;
+
+    if (N % 3 == 0) solution(N / 3, dp[N] + 1);
+    if (N % 2 == 0) solution(N / 2, dp[N] + 1);
+    solution(N - 1, dp[N] + 1);
   }
 
   public static void main(String[] args) throws IOException {
     BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     int N = Integer.parseInt(br.readLine());
+    dp = new int[N + 1];
+    Arrays.fill(dp, Integer.MAX_VALUE);
     solution(N, 0);
     System.out.println(answer);
   }
