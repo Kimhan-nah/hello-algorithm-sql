3d49abbb743c6927df6920b49638f468b1336017
Kimhannah <skqkdldhf98@naver.com>
2024-02-13 11:41:27 +0900

    :recycle: 2차원 배열로 수정

--- src/baekjoon/dynamic_programming/BJ11660.java
+++ src/baekjoon/dynamic_programming/BJ11660.java
@@ -5,13 +5,12 @@ import java.util.*;
 
 public class BJ11660 {
 	private static int N, M;
-	private static int[] arr;
+	private static int[][] arr;
 
 	private static void cumulativeSum() {
-		for (int i = 0; i < N; ++i) {
+		for (int i = 1; i <= N; ++i) {
 			for (int j = 1; j <= N; ++j) {
-				int index = i * N + j;
-				arr[index] += arr[index - 1];
+				arr[i][j] += arr[i][j - 1];
 			}
 		}
 	}
@@ -21,13 +20,13 @@ public class BJ11660 {
 		StringTokenizer st = new StringTokenizer(br.readLine());
 		N = Integer.parseInt(st.nextToken());
 		M = Integer.parseInt(st.nextToken());
-		arr = new int[N * N + 1];
+		arr = new int[N + 1][N + 1];
 		StringBuilder sb = new StringBuilder();
 
-		for (int i = 0; i < N; ++i) {
+		for (int i = 1; i <= N; ++i) {
 			st = new StringTokenizer(br.readLine());
 			for (int j = 1; j <= N; ++j) {
-				arr[i * N + j] = Integer.parseInt(st.nextToken());
+				arr[i][j] = Integer.parseInt(st.nextToken());
 			}
 		}
 		cumulativeSum();
@@ -38,8 +37,8 @@ public class BJ11660 {
 			int x2 = Integer.parseInt(st.nextToken());
 			int y2 = Integer.parseInt(st.nextToken());
 			int result = 0;
-			for (int j = x1 - 1; j < x2; ++j) {
-				result += (arr[j * N + y2] - arr[j * N + y1 - 1]);
+			for (int j = x1; j <= x2; ++j) {
+				result += (arr[j][y2] - arr[j][y1 - 1]);
 			}
 
 			sb.append(result).append("\n");
