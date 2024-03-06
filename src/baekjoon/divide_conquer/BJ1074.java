7c8d26f6457c15d8e8f7e057a15470838ef20bc1
Kimhannah <skqkdldhf98@naver.com>
2024-03-06 15:33:47 +0900

    :seedling: BaekJoon 1074번 Z 시간초과 개선

--- src/baekjoon/divide_conquer/BJ1074.java
+++ src/baekjoon/divide_conquer/BJ1074.java
@@ -5,25 +5,34 @@ import java.util.StringTokenizer;
 
 public class BJ1074 {
 	private static int n ,r, c;
-	private static int count = -1;
-	private static int[] dx = {0, 1, 0, 1};	// col
-	private static int[] dy = {0, 0, 1, 1}; // row
-	private static boolean isFinished;
+	private static int answer = 0;
 
-	private static void recursive(int row, int col, int length) {
-		if (isFinished) return;
-		if (length == 1) {
-			if (row == r && col == c) {
-				isFinished = true;
-			}
-			++count;
+	private static void recursive(int row, int col, int length, int count) {
+		if (row == r && col == c) {
+			answer = count;
 			return;
 		}
+		int dividedLength = length / 2;
 
-		int nextLength = length / 2;
-		for (int i = 0; i < 4; ++i) {
-			if (isFinished) return ;
-			recursive(row + dy[i] * nextLength, col + dx[i] * nextLength, nextLength);
+		if (row + length / 2 > r) {
+			if (col + length / 2 > c) {
+				// 1 사분면
+				recursive(row, col, dividedLength, count);
+			} else {
+				// 2 사분면
+				int tmp = dividedLength * dividedLength;
+				recursive(row, col + dividedLength, dividedLength, count + tmp);
+			}
+		} else {
+			if (col + length / 2 > c) {
+				// 3 사분면
+				int tmp = dividedLength * dividedLength * 2;
+				recursive(row + dividedLength, col, dividedLength, count + tmp);
+			} else {
+				// 4 사분면
+				int tmp = dividedLength * dividedLength * 3;
+				recursive(row + dividedLength, col + dividedLength, dividedLength, count + tmp);
+			}
 		}
 	}
 
@@ -35,8 +44,8 @@ public class BJ1074 {
 		c = Integer.parseInt(st.nextToken());
 		n = (int)Math.pow(2, n);
 
-		recursive(0, 0, n);
+		recursive(0, 0, n, 0);
 
-		System.out.println(count);
+		System.out.println(answer);
 	}
 }
