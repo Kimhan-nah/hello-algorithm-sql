518ec4fabf23f448f0c6b4679613fc249c2dd014
Kimhannah <skqkdldhf98@naver.com>
2024-03-08 13:58:24 +0900

    :bulb: BaekJoon 14888번 연산자 끼워넣기 개선

--- src/baekjoon/implementation/BJ14888.java
+++ src/baekjoon/implementation/BJ14888.java
@@ -6,8 +6,7 @@ import java.util.*;
 public class BJ14888 {
 	private static int n;
 	private static int[] sequence;
-	private static char[] operations;
-	private static boolean[] isUsed;
+	private static int[] operations;
 	private static int min = Integer.MAX_VALUE;
 	private static int max = Integer.MIN_VALUE;
 
@@ -18,29 +17,29 @@ public class BJ14888 {
 			return;
 		}
 
-		for (int i = 0; i < n - 1; ++i) {
-			if (isUsed[i]) {
-				continue;
+		for (int i = 0; i < 4; ++i) {
+			for (int j = 0; j < operations[i]; ++j) {
+				if (operations[i] < 1) continue;
+				--operations[i];
+				int nextCalculated = calculated;
+				int operand = sequence[level];
+				switch (i) {
+					case 0:
+						nextCalculated += operand;
+						break;
+					case 1:
+						nextCalculated -= operand;
+						break;
+					case 2:
+						nextCalculated *= operand;
+						break;
+					case 3:
+						nextCalculated /= operand;
+						break;
+				}
+				solution(nextCalculated, level + 1);
+				++operations[i];
 			}
-			isUsed[i] = true;
-			int nextCalculated = calculated;
-			int operand = sequence[level];
-			switch (operations[i]) {
-				case '+':
-					nextCalculated += operand;
-					break;
-				case '-':
-					nextCalculated -= operand;
-					break;
-				case '*':
-					nextCalculated *= operand;
-					break;
-				case '/':
-					nextCalculated /= operand;
-					break;
-			}
-			solution(nextCalculated, level + 1);
-			isUsed[i] = false;
 		}
 	}
 
@@ -48,23 +47,17 @@ public class BJ14888 {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		n = Integer.parseInt(br.readLine());
 		sequence = new int[n];
-		operations = new char[n - 1];
-		isUsed = new boolean[n - 1];
+		operations = new int[4];
 
 		StringTokenizer st = new StringTokenizer(br.readLine());
 		for (int i = 0; i < n; ++i) {
 			sequence[i] = Integer.parseInt(st.nextToken());
 		}
 		st = new StringTokenizer(br.readLine());
-		int plus = Integer.parseInt(st.nextToken());
-		int minus = Integer.parseInt(st.nextToken());
-		int multi = Integer.parseInt(st.nextToken());
-		int division = Integer.parseInt(st.nextToken());
-
-		Arrays.fill(operations, 0, plus, '+');
-		Arrays.fill(operations, plus, plus + minus, '-');
-		Arrays.fill(operations, plus + minus, plus + minus + multi, '*');
-		Arrays.fill(operations, plus + minus + multi, plus + minus + multi + division, '/');
+		operations[0] = Integer.parseInt(st.nextToken());
+		operations[1] =Integer.parseInt(st.nextToken());
+		operations[2] = Integer.parseInt(st.nextToken());
+		operations[3] =Integer.parseInt(st.nextToken());
 
 		solution(sequence[0], 1);
 
