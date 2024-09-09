549161d974bef31b1288f658b89464c402c49c3a
hannkim <skqkdldhf98@gmail.com>
2024-09-09 23:01:02 +0900

    [Bronze III] Title: 소음, Time: 100 ms, Memory: 14300 KB -BaekjoonHub

---
+++ 백준/Bronze/2935. 소음/소음.java
@@ -0,0 +1,22 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.math.BigInteger;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BigInteger a = new BigInteger(br.readLine());
+        char operator = br.readLine().charAt(0);
+        BigInteger b = new BigInteger(br.readLine());
+
+        switch (operator) {
+            case '*':
+                System.out.println(a.multiply(b));
+                break;
+            case '+':
+                System.out.println(a.add(b));
+                break;
+        }
+    }
+}
